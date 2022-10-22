9955a62f8b7f575ecf52858d57c9a74f1f1da8ff
HorraceMuendo <horracemuendo34@gmail.com>
2022-10-22 14:41:33 +0300

    backend v1

---
+++ src/test/java/com/example/webApp/WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
